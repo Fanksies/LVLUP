5bfdf00ca296251ae54f7e85a52efefe4009ccc7
Fanks <hola@marm.dev>
2021-11-11 19:03:47 -0600

    Added the thing

---
+++ src/Components/gallery.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import menu1 from '../Pictures/menu-1.png'
+import menu2 from '../Pictures/menu-2.png'
+import menu3 from '../Pictures/menu-3.png'
+
+const images = [
+    {
+      original: menu1,
+      thumbnail: menu1,
+    },
+    {
+      original: menu2,
+      thumbnail: menu2,
+    },
+    {
+      original: menu3,
+      thumbnail: menu3,
+    },
+  ];
+
+
+class Gallery extends React.Component {
+  render() {
+    return <ImageGallery items={images} />;
+  }
+}
+
+export default Gallery
